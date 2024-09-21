9ad60789ed99052f0e7c2a90e0b1a14b41e83505
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2024-09-22 02:18:53 +0530

    Time: 1 ms (100%), Space: 48.4 MB (50.3%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,37 +1,20 @@
 import java.util.*;
-import java.util.concurrent.Callable;
-import java.util.stream.Collectors;
 
 class Solution {
+    List<Integer> ret;
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> ret = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            ret.add(i);
-        }
-        Collections.sort(ret, new MyComparator());
+        ret = new ArrayList<>();
+        recursiveTrie(0, n);
         return ret;
     }
-}
 
-class MyComparator implements Comparator<Integer>{
-
-    @Override
-    public int compare(Integer o1, Integer o2) {
-        String a = o1 + "";
-        String b = o2+ "";
-        int lim = Math.min(a.length(), b.length());
-        int ag = 0;
-        for (int i = 0; i < lim; i++) {
-            if(Integer.parseInt(a.charAt(i) + "") > Integer.parseInt(b.charAt(i) + "")){
-                return 1;
-            }else if(Integer.parseInt(a.charAt(i) + "") < Integer.parseInt(b.charAt(i) + "")){
-                return -1;
+    private void recursiveTrie(int node, int n) {
+        for (int i = node;i<=Math.min(node+9, n);i++){
+            if(i==0) continue;
+            ret.add(i);
+            if(i*10 <= n){
+                recursiveTrie(i*10, n);
             }
-        }
-        if(a.length() > b.length()){
-            return 1;
-        }else {
-            return -1;
         }
     }
 }
