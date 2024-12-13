16968a78a98ed48584edb67d1e4c6c05d0f75c07
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2024-12-14 02:30:18 +0530

    Time: 109 ms (81.22%), Space: 61.2 MB (26.22%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -5,7 +6,37 @@ import java.util.Queue;
 class Solution {
     public long findScore(int[] nums) {
         long score = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+//        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] o1, int[] o2) {
+//                if(o1[0] == o2[0]){
+//                    return o1[1] - o2[1];
+//                }
+//                return o1[0] - o2[0];
+//            }
+//        });
+        int[][] sortedMin = new int[nums.length][2];
+        for (int i = 0; i < nums.length; i++) {
+            //pq.add(new int[]{nums[i], i});
+            sortedMin[i][0] = nums[i];
+            sortedMin[i][1] = i;
+        }
+        int[] marked = new int[nums.length];
+//        while (!pq.isEmpty()){
+//            int[] min = pq.poll();
+//            if(marked[min[1]] == 1){
+//                continue;
+//            }
+//            score += min[0];
+//            marked[min[1]] = 1;
+//            if(min[1] - 1 >=0){
+//                marked[min[1] - 1] = 1;
+//            }
+//            if(min[1] + 1 < nums.length){
+//                marked[min[1] + 1] = 1;
+//            }
+//        }
+        Arrays.sort(sortedMin, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
                 if(o1[0] == o2[0]){
@@ -14,12 +45,8 @@ class Solution {
                 return o1[0] - o2[0];
             }
         });
-        for (int i = 0; i < nums.length; i++) {
-            pq.add(new int[]{nums[i], i});
-        }
-        int[] marked = new int[nums.length];
-        while (!pq.isEmpty()){
-            int[] min = pq.poll();
+        for (int i=0; i<nums.length; i++){
+            int[] min = sortedMin[i];
             if(marked[min[1]] == 1){
                 continue;
             }
