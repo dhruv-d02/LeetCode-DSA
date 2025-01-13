734dc26a0e687dba995585b4a0e9f33a22284a9d
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2025-01-14 00:34:48 +0530

    Time: 10 ms (82.12%), Space: 46.2 MB (75.5%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -4,27 +4,19 @@ import java.util.PriorityQueue;
 
 class Solution {
     public int minimumLength(String s) {
-        PriorityQueue[] charIdx = new PriorityQueue[26];
-        for (int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            int idx = c - 'a';
-            if(charIdx[idx] == null){
-                charIdx[idx] = new PriorityQueue();
-            }
-            charIdx[idx].add(i);
+        int[] charOccurence = new int[26];
+        for (char c : s.toCharArray()){
+            charOccurence[c-'a']++;
         }
         int cnt = 0;
-        for (PriorityQueue<Integer> arr : charIdx){
-            if(arr == null){
+        for(int occ : charOccurence){
+            if(occ ==0){
                 continue;
+            }
+            if(occ%2 == 0){
+                cnt += 2;
             }else{
-                while (arr.size() >= 3){
-                    int first = arr.poll();
-                    int second = arr.poll();
-                    int third = arr.poll();
-                    arr.add(second);
-                }
-                cnt += arr.size();
+                cnt+=1;
             }
         }
         return cnt;
