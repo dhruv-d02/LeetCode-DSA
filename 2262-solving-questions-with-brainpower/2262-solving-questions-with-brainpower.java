da5300ad66e9734df58b38171052890f47128ae2
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2025-04-03 02:07:56 +0530

    Time: 6 ms (70.74%), Space: 108 MB (89.47%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -1,7 +1,8 @@
 class Solution {
     public long mostPoints(int[][] questions) {
         long[] dp = new long[questions.length];
-        for (int i = questions.length - 1; i >= 0; i--) {
+        dp[questions.length - 1] = questions[questions.length - 1][0];
+        for (int i = questions.length - 2; i >= 0; i--) {
             int index = i + questions[i][1] + 1;
             if (index < questions.length) {
                 dp[i] = dp[index] + questions[i][0];
