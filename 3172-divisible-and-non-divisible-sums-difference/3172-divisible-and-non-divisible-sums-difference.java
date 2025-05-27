926b349add3a2203d9673806adf762ba2379bbc4
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2025-05-27 23:21:01 +0530

    Time: 0 ms (100%), Space: 40.8 MB (68.69%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.java
@@ -1,15 +1,9 @@
 class Solution {
     public int differenceOfSums(int n, int m) {
+        int k = n/m; //count of how many numbers are there as multiple of m in [1,n];
+        return ((n * (n + 1)) / 2) - (m * k * (k + 1)); //Sum of n numbers - 2*sum of AP with difference m
 
-        int num1 = 0;
-        int num2 = 0;
-        for(int i=1; i<=n; i++){
-            if(i%m == 0){
-                num2 += i;
-            }else{
-                num1 += i;
-            }
-        }
-        return num1-num2;
+        //3+6+9 => 3(1+2+3) => 3*(3*(3+1)/2)
+        //Multiplied by 2 because it's sum was added in Sum of n numbers as well so subtracting it twice;
     }
 }
