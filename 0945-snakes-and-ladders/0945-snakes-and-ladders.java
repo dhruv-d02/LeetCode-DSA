2061d16e9a81083395455db56097cc46e4a4810b
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2025-06-01 03:09:48 +0530

    Time: 4 ms (95.14%), Space: 44.8 MB (67.7%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.java
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.java
@@ -1,27 +1,35 @@
+import java.util.*;
+
 class Solution {
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
-        int[] min_rolls = new int[n * n + 1];
-        Arrays.fill(min_rolls, -1);
-        Queue<Integer> q = new LinkedList<>();
-        min_rolls[1] = 0;
-        q.offer(1);
+        if(n*n <= 6){
+            return 1;
+        }
 
-        while (!q.isEmpty()) {
-            int x = q.poll();
-            for (int i = 1; i <= 6 && x + i <= n * n; i++) {
-                int t = x + i;
-                int row = (t - 1) / n;
-                int col = (t - 1) % n;
-                int v = board[n - 1 - row][(row % 2 == 1) ? (n - 1 - col) : col];
-                int y = (v > 0 ? v : t);
-                if (y == n * n) return min_rolls[x] + 1;
-                if (min_rolls[y] == -1) {
-                    min_rolls[y] = min_rolls[x] + 1;
-                    q.offer(y);
+        int[] min_rolls = new int[(n+1)*(n+1)];
+        Arrays.fill(min_rolls, -1);
+        min_rolls[1] = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1);
+
+        while (!queue.isEmpty()){
+            int top = queue.poll();
+
+            for(int i=1; i<=6 && top + i <= n*n; i++){
+                int curr = top+i;
+                int row = (curr-1)/n;
+                int col = (curr-1)%n;
+                int boardVal = board[n-1-row][row%2==0 ? col : n-1-col];
+                int to = boardVal > 0 ? boardVal : curr;
+                if(to == n*n) return min_rolls[top] + 1;
+                if(min_rolls[to] == -1){
+                    min_rolls[to] = min_rolls[top] + 1;
+                    queue.offer(to);
                 }
             }
         }
         return -1;
     }
+
 }
