b251d0a443bdcc6ce1a27020f88f0d76ad6dd7f1
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2025-06-04 02:53:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.java
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.java
@@ -1,55 +1,52 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 
 class Solution {
     public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
-        int n = status.length;
-        boolean[] visited = new boolean[n];
-        boolean[] boxOwned = new boolean[n];
-        boolean[] boxOpened = new boolean[n];
-        Set<Integer> keySet = new HashSet<>();
 
+        boolean[] canVisit;
+        Set<Integer> keysFound;
+        int n = candies.length;
+        canVisit = new boolean[n];
+        keysFound = new HashSet<>();
         Queue<Integer> queue = new LinkedList<>();
+        for(int box : initialBoxes)
+            queue.add(box);
 
-        for (int box : initialBoxes) {
-            boxOwned[box] = true;
-            queue.offer(box);
-        }
+        int total = 0;
+        while (!queue.isEmpty()){
+            int curr = queue.poll();
+            if(curr == -1)
+                continue;
+            if(canVisit[curr]){
+                continue;
+            }
 
-        int totalCandies = 0;
+            canVisit[curr] = true;
+            if(status[curr] == 0 && !keysFound.contains(curr)){
+                queue.add(curr);
+                continue;
+            }
+            if(status[curr] == 1 || keysFound.contains(curr)){
+                total += candies[curr];
+            }
 
-        boolean progress = true;
-        while (progress) {
-            progress = false;
-            int size = queue.size();
-
-            for (int i = 0; i < size; i++) {
-                int box = queue.poll();
-
-                // Can open only if already open or we have the key
-                if (status[box] == 1 || keySet.contains(box)) {
-                    if (visited[box]) continue;
-
-                    visited[box] = true;
-                    progress = true;
-
-                    totalCandies += candies[box];
-
-                    for (int k : keys[box]) {
-                        keySet.add(k);
-                        if (boxOwned[k]) queue.offer(k); // retry if we already own the box
-                    }
-
-                    for (int b : containedBoxes[box]) {
-                        boxOwned[b] = true;
-                        queue.offer(b); // add to queue to process
-                    }
-                } else {
-                    // Put it back in the queue to retry later if we get its key
-                    queue.offer(box);
+            for(int key : keys[curr]){
+                if(key != -1 && canVisit[key]) {
+                    if(!keysFound.contains(key))
+                        canVisit[key] = false;
+                    queue.add(key);
                 }
+                keysFound.add(key);
+            }
+
+            for(int box : containedBoxes[curr]){
+                queue.add(box);
             }
         }
 
-        return totalCandies;
+        return total;
     }
 }
