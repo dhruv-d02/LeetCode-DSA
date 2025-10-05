121981453caa4fa8f1b36fa4a6bf296306d09bd7
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2025-10-06 01:22:46 +0530

    Time: 0 ms (100%), Space: 41.2 MB (20.81%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -2,16 +2,12 @@ import java.io.StringReader;
 
 class Solution {
     public String convertToTitle(int n) {
-        if(n==0){
-            return "Z";
-        }
-        if(n <= 26){
-            return (char)('A'+n-1) + "";
-        }
-        if(n%26 == 0){
-            return convertToTitle(n/26 - 1) + 'Z';
-        }
-        String ret = convertToTitle(n/26)+ convertToTitle(n%26);
-        return ret;
+        StringBuilder sb = new StringBuilder();
+       while (n > 0){
+           n--;
+           sb.insert(0, (char) ('A'+n%26));
+           n /= 26;
+       }
+       return sb.toString();
     }
 }
