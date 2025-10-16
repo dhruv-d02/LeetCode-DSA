1c21b5c1fe46d06618f22eff752805226105a309
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2025-10-17 01:07:52 +0530

    Time: 3 ms (22.06%), Space: 58.4 MB (53.43%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.java
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.java
@@ -12,19 +12,26 @@
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
         ListNode tempHead = head;
+        ListNode fromBeg = head;
+        ListNode fromLast = head;
+        int size = 1;
+        while (tempHead.next  != null){
+            size++;
+            tempHead = tempHead.next;
+        }
+        tempHead = head;
         int pos = 1;
-        HashMap<Integer, ListNode> map = new HashMap<>();
-        while (tempHead  != null){
-            map.put(pos, tempHead);
+        //System.out.println(size);
+        while (tempHead != null){
+            if(pos == k){
+                fromBeg = tempHead;
+            }
+            if(pos == size-k+1){
+                fromLast = tempHead;
+            }
             pos++;
             tempHead = tempHead.next;
         }
-        //         for(Map.Entry<Integer, ListNode> e : map.entrySet()){
-        //     System.out.println(e.getKey() + "  " + e.getValue().val);
-        // }
-        ListNode fromBeg = map.get(k);
-        ListNode fromLast = map.get(map.size() - k+1);
-        // System.out.println(map.size() + "  " + fromBeg.val + "  " + fromLast.val);
         int temp = fromLast.val;
         fromLast.val = fromBeg.val;
         fromBeg.val = temp;
