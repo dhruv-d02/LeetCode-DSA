2ed54c9fc370556e9d48487e734e8f00defe238f
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2026-02-14 01:43:15 +0530

    Time: 648 ms (13.49%), Space: 47.2 MB (7.6%) - LeetHub

---
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int longestBalanced(String s) {
+        Map<Character, Integer> mp = new HashMap<>();
+        int sz = s.length(), ans = 0;
+
+        for (int i = 0; i < sz; i++) {
+            mp.clear(); // Emptying the hash map for the new starting point
+            for (int j = i; j < sz; j++) {
+                char c = s.charAt(j);
+                mp.put(c, mp.getOrDefault(c, 0) + 1);
+                
+                int cnt = mp.get(c);
+                boolean flag = true;
+                
+                for (int count : mp.values()) {
+                    if (count != cnt) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    ans = Math.max(ans, j - i + 1);
+                }
+            }
+        }
+        return ans;
+    }
+}
