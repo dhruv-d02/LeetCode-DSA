1c040c8886fac325f4f650a634147df42db839ec
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2026-03-31 00:19:10 +0530

    Time: 3 ms (100%), Space: 47.9 MB (85.53%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,16 +1,28 @@
 class Solution {
     public boolean checkStrings(String s1, String s2) {
-        int[] freq = new int[52];
-
-        for (int i = 0; i < s1.length(); i++) {
-            int off = (i & 1) * 26;
-            freq[s1.charAt(i) - 'a' + off]++;
-            freq[s2.charAt(i) - 'a' + off]--;
+        int  n = s1.length();
+        int freq1[]=new int[26];
+        for(int i=0;i<n;i+=2){
+            freq1[s1.charAt(i)-'a']++;
         }
-
-        for (int i = 0; i < 52; i++)
-            if (freq[i] != 0) return false;
-
+        int freq2[]=new int[26];
+        for(int i=0;i<n;i+=2){
+            freq2[s2.charAt(i)-'a']++;
+        }
+        for(int i =0;i<26;i++){
+            if(freq1[i]!=freq2[i])return false;
+            freq1[i]=0;
+            freq2[i]=0;
+        }
+        for(int i=1;i<n;i+=2){
+            freq1[s1.charAt(i)-'a']++;
+        }
+        for(int i=1;i<n;i+=2){
+            freq2[s2.charAt(i)-'a']++;
+        }
+        for(int i =0;i<26;i++){
+            if(freq1[i]!=freq2[i])return false;
+        }
         return true;
     }
 }
