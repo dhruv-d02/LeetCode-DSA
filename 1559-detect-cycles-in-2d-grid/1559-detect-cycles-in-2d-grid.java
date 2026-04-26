577da2249c85e01c2851cee2a2d403b750dff9ec
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2026-04-27 00:33:32 +0530

    Time: 19 ms (63.88%), Space: 141.9 MB (32.67%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -1,48 +1,52 @@
 class Solution {
-    int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
-    
+    int[][] dir = new int[][]{{0,1}, {0,-1}, {1,0}, {-1, 0}};
     public boolean containsCycle(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        
-        boolean[][] visited = new boolean[m][n];
-        
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(!visited[i][j]) {
-                    if(dfs(grid, i, j, -1, -1, visited)) {
+        boolean[][] vis  = new boolean[m][n];
+
+        for(int i=0; i<m; i++)
+            for (int j = 0; j <n; j++) {
+                if(!vis[i][j]){
+                    if(dfs(i, j, -1, -1, vis, grid))
                         return true;
-                    }
                 }
             }
-        }
-        
         return false;
     }
-    
-    private boolean dfs(char[][] grid, int x, int y, int parentX, int parentY, boolean[][] visited) {
-        visited[x][y] = true;
-        
-        for(int[] dir : directions) {
-            int nx = x + dir[0];
-            int ny = y + dir[1];
-            
-            if(nx < 0 || ny < 0 || nx >= grid.length || ny >= grid[0].length)
+
+    private boolean dfs(int i, int j, int pX, int pY, boolean[][] vis, char[][] grid) {
+        vis[i][j] = true;
+
+        for(int[] d : dir){
+            int nx = i + d[0];
+            int ny = j + d[1];
+            if(!valid(nx, ny, grid.length, grid[0].length))
                 continue;
-            
-            if(grid[nx][ny] != grid[x][y])
+
+            if(nx == pX && ny == pY)
                 continue;
-            
-            if(nx == parentX && ny == parentY)
+
+            if(grid[nx][ny] != grid[i][j])
                 continue;
-            
-            if(visited[nx][ny])
+
+            if(vis[nx][ny]){
                 return true;
-            
-            if(dfs(grid, nx, ny, x, y, visited))
+            }
+
+            if(dfs(nx, ny, i, j, vis, grid))
                 return true;
+
         }
-        
+
         return false;
+    }
+
+    private boolean valid(int x, int y, int m, int n) {
+        if(x < 0 || y < 0)
+            return false;
+        if(x >= m || y >= n)
+            return false;
+        return true;
     }
 }
