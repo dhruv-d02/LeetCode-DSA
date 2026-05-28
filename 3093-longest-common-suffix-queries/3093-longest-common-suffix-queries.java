0224966fc4ddcafcc97198e68efe845d55509f09
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2026-05-29 01:56:38 +0530

    Time: 55 ms (61.08%), Space: 284.3 MB (22.16%) - LeetHub

--- 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.java
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.java
@@ -1,92 +1,48 @@
-import com.sun.jdi.connect.AttachingConnector;
-
-import javax.sql.rowset.spi.SyncResolver;
-import java.util.*;
-
 class Solution {
-    HashMap<String, List<Integer>> map;
     public int[] stringIndices(String[] wordsContainer, String[] wordsQuery) {
         TrieNode root = new TrieNode();
-        map = new HashMap<>();
-        for (int i=0; i<wordsContainer.length; i++){
-            root.add(wordsContainer[i]);
-            root.stringList.add(wordsContainer[i]);
-            if(map.get(wordsContainer[i]) == null)
-                map.put(wordsContainer[i], new ArrayList<>());
-            map.get(wordsContainer[i]).add(i);
-        }
-
-        int[] ret = new int[wordsQuery.length];
-
-        for (int i=0; i<wordsQuery.length; i++){
-            String res = root.query(wordsQuery[i]);
-            ret[i] = map.get(res).get(0);
-        }
-
-        return ret;
-    }
-    class TrieNode{
-        TrieNode[] nodes;
-        ArrayList<String> stringList;
-
-        public TrieNode() {
-            nodes = new TrieNode[26];
-            stringList = new ArrayList<>();
-        }
-
-        public void add(String s){
-            add(s, s.length()-1);
-        }
-
-        private void add(String s, int pos){
-            if(pos <0){
-                return;
-            }
-            if(this.nodes[s.charAt(pos)-'a'] == null){
-                this.nodes[s.charAt(pos)-'a'] = new TrieNode();
-            }
-            this.nodes[s.charAt(pos)-'a'].add(s, pos-1);
-            this.nodes[s.charAt(pos)-'a'].stringList.add(s);
-        }
-
-        public String query(String q) {
-            TrieNode lastNodeFound = query(q, q.length());
-            if(lastNodeFound == null){
-                //no suffix found
-                lastNodeFound = this;
+        for (int i = 0; i < wordsContainer.length; i++) {
+            String word = wordsContainer[i];
+            int len = word.length();
+            TrieNode curr = root;
+            
+            if(curr.best_len > len || (curr.best_len == len && curr.best_idx > i)){
+                curr.best_idx = i;
+                curr.best_len = len;
             }
 
-            lastNodeFound.stringList.sort(new Comparator<String>() {
-                @Override
-                public int compare(String o1, String o2) {
-                    return o1.length() - o2.length();
+            for (int j = len-1; j >= 0; j--) {
+                if(curr.nodes[word.charAt(j)-'a'] == null){
+                    curr.nodes[word.charAt(j)-'a'] = new TrieNode();
                 }
-            });
-            String min = lastNodeFound.stringList.get(0);
-
-            for(int i=0; i<lastNodeFound.stringList.size(); i++){
-                String s = lastNodeFound.stringList.get(i);
-                if(min.length() > s.length()){
-                    min = s;
-                }else if(min.length() == s.length() && map.get(min).get(0) > map.get(s).get(0)){
-                    min = s;
+                curr = curr.nodes[word.charAt(j)-'a'];
+                
+                if(curr.best_len > len || (curr.best_len == len && curr.best_idx > i)){
+                    curr.best_idx = i;
+                    curr.best_len = len;
                 }
             }
-            return min;
         }
-
-        private TrieNode query(String s, int pos){
-            if(pos < 0){
-                return null;
+        
+        int[] res = new int[wordsQuery.length];
+        
+        for (int i = 0; i < wordsQuery.length; i++) {
+            TrieNode curr = root;
+            String word = wordsQuery[i];
+            int len = word.length();
+            for (int j = len-1; j >= 0 ; j--) {
+                if(curr.nodes[word.charAt(j)-'a'] == null)
+                    break;
+                curr = curr.nodes[word.charAt(j)-'a'];
             }
-            if(pos == 0 || this.nodes[s.charAt(pos-1)-'a'] == null){
-                return this;
-            }
-            TrieNode res = this.nodes[s.charAt(pos-1)-'a'].query(s, pos-1);
-            if(res == null){
-                return this;
-            }
-            return res;
+            res[i] = curr.best_idx;
         }
+        
+        return res;
     }
+}
+class TrieNode{
+    TrieNode[] nodes = new TrieNode[26];
+    int best_len = Integer.MAX_VALUE;
+    int best_idx = Integer.MAX_VALUE;
 }
