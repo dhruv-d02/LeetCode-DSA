f83d2da88ed9314755d25cecafe3a9b55be55ff8
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2026-06-22 01:18:20 +0530

    Time: 36 ms (77.72%), Space: 84.2 MB (15.98%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -4,20 +4,32 @@ import java.util.TreeMap;
 
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        TreeMap<Integer, Integer> freqMap = new TreeMap<>();
+        // TreeMap<Integer, Integer> freqMap = new TreeMap<>();
 
-        for(int cost : costs){
-            freqMap.put(cost, freqMap.getOrDefault(cost, 0) + 1);
-        }
+        // for(int cost : costs){
+        //     freqMap.put(cost, freqMap.getOrDefault(cost, 0) + 1);
+        // }
 
+        // int count = 0;
+        // for(Map.Entry<Integer, Integer> entry : freqMap.entrySet()){
+        //     int cost = entry.getKey();
+        //     int freq = entry.getValue();
+        //     int maxBuy = Math.min(freq,coins/cost);
+        //     count += maxBuy;
+        //     coins -= maxBuy*cost;
+        // }
+        // return count;
+
+
+        Arrays.sort(costs);
         int count = 0;
-        for(Map.Entry<Integer, Integer> entry : freqMap.entrySet()){
-            int cost = entry.getKey();
-            int freq = entry.getValue();
-            int maxBuy = Math.min(freq,coins/cost);
-            count += maxBuy;
-            coins -= maxBuy*cost;
+        for(int cost : costs){
+            if(coins < cost)
+                break;
+            coins -= cost;
+            count++;
         }
+
         return count;
     }
 }
