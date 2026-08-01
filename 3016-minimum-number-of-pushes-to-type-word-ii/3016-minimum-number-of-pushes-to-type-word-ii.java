8bf45d49d88f82ff8325a2c179a58766be298939
dhruv-d02 <153858743+dhruv-d02@users.noreply.github.com>
2026-08-02 00:24:32 +0530

    Time: 9 ms (91.52%), Space: 48.3 MB (18.47%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,25 @@
+import java.awt.color.CMMException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+class Solution {
+    public int minimumPushes(String word) {
+
+        int[] freq = new int[26];
+        for(char c : word.toCharArray())
+            freq[c-'a']++;
+
+
+
+        Arrays.sort(freq);
+        int ans = 0;
+
+        for(int i=25; i>=0; i--){
+            ans += freq[i]*((25-i)/8 + 1);
+        }
+        
+        return ans;
+    }
+}
